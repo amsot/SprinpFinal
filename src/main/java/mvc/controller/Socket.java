67e67140c5411edae17af5084096b9002fb40ed7
hyun <amsot@naver.com>
2018-05-29 12:12:05 +0900

    chat db 저장

--- src/main/java/mvc/controller/Socket.java
+++ src/main/java/mvc/controller/Socket.java
@@ -7,13 +7,23 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.socket.WebSocketSession;
+
+import mvc.dao.ChatDao;
+import mvc.dto.ChatDto;
+import mvc.service.ChatService;
+import mvc.service.ChatServiceImpl;
 
 @Controller
 public class Socket {
 
+	
 	private List<String> userList;
 	private Map<String,List<String>> chatRoomUser;
 	private List<String> chatRoomList;
@@ -24,6 +34,8 @@ public class Socket {
 	}
 
 
+	@Autowired ChatService s;
+	
 	@RequestMapping("/login")
 	public String login() {
 		return "chatting/login";
@@ -34,11 +46,9 @@ public class Socket {
 		session.setAttribute("id", id);
 		session.setAttribute("chatRoom", chatRoom);
 
-
 		if(!chatRoomList.contains(chatRoom)) {
 			chatRoomList.add(chatRoom);
 		}
-
 		if(chatRoomList.size() != chatRoomUser.size()) {
 			chatRoomUser.put(chatRoom, new ArrayList<String>());
 			chatRoomUser.get(chatRoom).add(id);
@@ -56,6 +66,25 @@ public class Socket {
 
 		return "chatting/chat";
 	}
+	
+	@ResponseBody
+	@RequestMapping(value="/chat/db",method = RequestMethod.POST)
+	public String chatdb(@RequestBody String message,HttpSession session) {
+		
+		ChatDto dto = new ChatDto();
+		System.out.println(message);
+		System.out.println((String)session.getAttribute("chatRoom"));
+		System.out.println((String)session.getAttribute("id"));
+		dto.setChat_content(message);
+		dto.setChatroom_id((String)session.getAttribute("chatRoom"));
+		dto.setUESR_ID((String)session.getAttribute("id"));
+		
+		s.insertChat(dto);
+
+		return "hihi";
+		
+	}
+
 
 
 }
--- src/main/java/mvc/dto/ChatDto.java
+++ src/main/java/mvc/dto/ChatDto.java
@@ -4,40 +4,48 @@ import java.util.Date;
 
 public class ChatDto {
 
-  private String chat_no;
-  private String  chat_content;
-  private Date chat_sendTime;
-  private String  chatroom_id;
-   public String getChat_no() {
-	return chat_no;
-}
-public void setChat_no(String chat_no) {
-	this.chat_no = chat_no;
-}
-public String getChat_content() {
-	return chat_content;
-}
-public void setChat_content(String chat_content) {
-	this.chat_content = chat_content;
-}
-public Date getChat_sendTime() {
-	return chat_sendTime;
-}
-public void setChat_sendTime(Date chat_sendTime) {
-	this.chat_sendTime = chat_sendTime;
-}
-public String getChatroom_id() {
-	return chatroom_id;
-}
-public void setChatroom_id(String chatroom_id) {
-	this.chatroom_id = chatroom_id;
-}
-public int getUesr_no() {
-	return uesr_no;
-}
-public void setUesr_no(int uesr_no) {
-	this.uesr_no = uesr_no;
-}
-int uesr_no;
+	private String chat_no;
+	private String  chat_content;
+	private Date chat_sendTime;
+	private String  chatroom_id;
+	private String UESR_ID;
 	
+	public String getUESR_ID() {
+		return UESR_ID;
+	}
+	public void setUESR_ID(String uESR_ID) {
+		UESR_ID = uESR_ID;
+	}
+	public String getChat_no() {
+		return chat_no;
+	}
+	public void setChat_no(String chat_no) {
+		this.chat_no = chat_no;
+	}
+	public String getChat_content() {
+		return chat_content;
+	}
+	public void setChat_content(String chat_content) {
+		this.chat_content = chat_content;
+	}
+	public Date getChat_sendTime() {
+		return chat_sendTime;
+	}
+	public void setChat_sendTime(Date chat_sendTime) {
+		this.chat_sendTime = chat_sendTime;
+	}
+	public String getChatroom_id() {
+		return chatroom_id;
+	}
+	public void setChatroom_id(String chatroom_id) {
+		this.chatroom_id = chatroom_id;
+	}
+	public int getUesr_no() {
+		return uesr_no;
+	}
+	public void setUesr_no(int uesr_no) {
+		this.uesr_no = uesr_no;
+	}
+	int uesr_no;
+
 }
